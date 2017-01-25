86b57063e4d823fa86fab3bfc5df876eb0345e6e
xuewenyao <bresai@live.cn>
2017-01-25 14:06:05 +0800

    week2 6 finished

--- week2-6/src/main/java/TSP.java
+++ week2-6/src/main/java/TSP.java
@@ -15,6 +15,7 @@ public class TSP {
     public TSP(String filename) {
         try {
             readFormFile(filename);
+            initDistance();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -33,13 +34,21 @@ public class TSP {
         }
     }
 
+    private void initDistance(){
+        for (int i = 0; i < length; i++){
+            for (int j = 0; j < length; j++){
+                distanceCache[i][j] = list.get(i).distance(list.get(j));
+            }
+        }
+    }
+
     public void combination(int n, int total, int begin, int result) {
         if (n == 0) {//如果够n个数了，输出
             sList.add(result);
             return;
         }
 
-        for (int i = begin; i <= total; i++) {
+        for (int i = begin; i < total; i++) {
             combination(n - 1, total, i + 1, 1 << i | result);
         }
     }
@@ -47,81 +56,55 @@ public class TSP {
     public Double tsp() {
         Map<Integer, Map<Integer, Double>> mapCached = new HashMap<>();
 
-        for (int i = 1; i <= length; i++){
-            Integer tmp = 1 << i - 1;
-            Map<Integer, Double> tMap = new HashMap<>();
-            tMap.put(i, i == 1 ? 0 : Double.MAX_VALUE);
-            mapCached.put(tmp, tMap);
-        }
+        Map<Integer, Double> tMap = new HashMap<>();
+        tMap.put(0, 0.0);
+        mapCached.put(1,tMap);
 
         for (int k = 2; k <= length; k++) {
-            Long tStart = System.currentTimeMillis();
             System.out.println(k);
-
             //get all combination of S;
             sList = new LinkedList<>();
-            combination(k - 1, length, 2, 1);
-            System.out.println(sList.size());
+            combination(k - 1, length, 1, 1);
             Map<Integer, Map<Integer, Double>> map = new HashMap<>(sList.size());
-            Map<Integer, Double> minCached = new HashMap<>(sList.size());
             for (Integer num : sList) {
                 if (! map.containsKey(num)){
                     map.put(num, new HashMap<>());
                 }
 
                 for (int j = 1; j < k; j++){
-                    Integer tmp = removeElement(num, j);
-
-                    Double res;
-                    if (minCached.containsKey(tmp)){
-                        res = minCached.get(tmp);
-                    }else{
-                        res = findMin(j, mapCached.get(tmp));
-                        minCached.put(tmp, res);
+                    int index = 0;
+                    Integer iTmp = num;
+                    int shift = 0;
+                    while (index < j){
+                        iTmp = (iTmp >> 1);
+                        shift++;
+                        if ((iTmp  & 1) == 1){
+                            index++;
+                        }
                     }
-                    map.get(num).put(j, res);
+                    Integer tmp =  1 << shift ^ num;
+
+                    Double res = findMin(shift, mapCached.get(tmp));
+
+                    map.get(num).put(shift, res);
                 }
             }
 
             mapCached = map;
-            System.out.println("time used:" + (System.currentTimeMillis() - tStart));
         }
 
         for (Map<Integer,Double> map : mapCached.values()){
-            return findMin(1, map);
+            return findMin(0, map);
         }
 
         return null;
-    }
-
-    private Integer removeElement(int num, int j){
-        int index = 0;
-        Integer iTmp = num;
-        int shift = 0;
-        while (index < j){
-            iTmp = (iTmp >> 1);
-            shift++;
-            if ((iTmp  & 1) == 1){
-                index++;
-            }
-        }
-        return 1 << shift ^ num;
     }
 
     private Double findMin(int j, Map<Integer, Double> cached){
         Double min = Double.MAX_VALUE;
         for (int key : cached.keySet()){
             if (key == j) continue;
-            Location p1 = list.get(key - 1);
-            Location p2 = list.get(j - 1);
-            Double distance;
-            if (distanceCache[key - 1][j - 1] != null){
-                distance = cached.get(key) + distanceCache[key - 1][j - 1];
-            } else{
-                distance = cached.get(key) + p1.distance(p2);
-                distanceCache[key - 1][j - 1] = p1.distance(p2);
-                distanceCache[j - 1][key - 1] = p1.distance(p2);
-            }
+            double distance = cached.get(key) + distanceCache[key][j];
             min = distance < min ? distance : min;
         }
         return min;
@@ -130,11 +113,10 @@ public class TSP {
     public static void main(String[] args) {
         TSP tsp = new TSP("week2-6/src/main/resources/tsp.txt");
         System.out.println(tsp.tsp());
-//        List<Integer> result = new ArrayList<>(5);
-//        Integer result = 1;
-        //get all combination of S;
+        Integer result = 1;
+//        //get all combination of S;
 //        tsp.sList = new LinkedList<>();
-//        tsp.combination(1, 3, 1, result);
+//        tsp.combination(1, 3, 0, result);
 //        System.out.println(tsp.tsp());
     }
 }
